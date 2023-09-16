208fc28ee3953e89b71fdda1274f1aee8f332fda
rp-guruh <github.gurh@gmail.com>
2023-09-16 14:57:56 +0700

    fix json get journal by id

--- app/controllers/api/v1/journal_controller.rb
+++ app/controllers/api/v1/journal_controller.rb
@@ -17,13 +17,16 @@ class Api::V1::JournalController < Api::V1::AuthenticatedController
   def getJournalByID
     journal_id = params[:id]
     data = Journal.where(journal_id: journal_id).first
-    name_journal = JournalSetting.where(journal_id: journal_id).where(locale: "en_US").where.not(setting_value: nil).where(setting_name: "name").first
+    name_journal = JournalSetting.where(journal_id: journal_id).where(locale: data.primary_locale).where.not(setting_value: nil).where(setting_name: "name").first
+    about_journal = JournalSetting.where(journal_id: journal_id).where(locale: data.primary_locale).where.not(setting_value: nil).where(setting_name: "about").first
+    
     
    	result_hash = {
+   		id: data.journal_id,
 		  acronym: data.path,
-		  id: data.journal_id,
+		  fullname: name_journal.setting_value,
 		  locale: data.primary_locale,
-		  fullname: name_journal.setting_value
+		  about: about_journal.setting_value
 		}
 
 		# Mengkonversi hash ke format JSON
